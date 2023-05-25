98c74dc9594d93dadbfd0ee2e1c9adf6ae36412e
AnyJulian <julian.giraud1369@gmail.com>
2023-05-25 23:58:42 +0200

    V2

--- src/components/tacheProgress.js
+++
@@ -1,8 +0,0 @@
-// import ProgressBar from 'react-bootstrap/ProgressBar';
-
-// function ProgressBare() {
-//   const now = 60;
-//   return <ProgressBar now={now} label={`${now}%`} />;
-// }
-
-// export default ProgressBare;
